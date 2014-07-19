5d9372c4dbc369da99973dfd6b7c72cdab05e00f
Amadeus <gliheng@gmail.com>
2014-07-19 19:00:23 +0800

    rebuilding site Sat Jul 19 19:00:23 CST 2014

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,11 @@ git add -A
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
-	  then msg="$1"
-	  fi
-	  git commit -m "$msg"
-	  
-	  # Push source and build repos.
-	  git push origin master
-	  git subtree push --prefix=public git@github.com:gliheng/gliheng.github.io.git master
+    then msg="$1"
+fi
+
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+git subtree push --prefix=public git@github.com:gliheng/gliheng.github.io.git master
