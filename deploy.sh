f7fb574a6efbd9959a69d8b8672103e517b5ff42
Amadeus <gliheng@gmail.com>
2015-01-31 15:56:07 +0800

    rebuilding site Sat Jan 31 15:56:07 CST 2015

--- deploy.sh
+++ deploy.sh
@@ -17,5 +17,5 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
+git push origin src
 git subtree push --prefix=public git@github.com:gliheng/gliheng.github.io.git master
